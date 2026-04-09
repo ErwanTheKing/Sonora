d64a35b0bccd6208f8ff53157e666edc391da5e5
ErwanTheGoat <erwan.lo-presti@epitech.eu>
2026-04-09 06:10:54 +0200

    feat(data): add local sample tracks list

---
+++ lib/models/track.dart
@@ -0,0 +1,13 @@
+class Track {
+  final String id;
+  final String title;
+  final String artist;
+  final String genre;
+
+  const Track({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.genre,
+  });
+}
---
+++ lib/services/track_data.dart
@@ -0,0 +1,94 @@
+import 'package:sonora/models/track.dart';
+
+final List<Track> sampleTracks = [
+  const Track(
+    id: '1',
+    title: 'Blinding Lights',
+    artist: 'The Weeknd',
+    genre: 'Pop',
+  ),
+  const Track(
+    id: '2',
+    title: 'HUMBLE.',
+    artist: 'Kendrick Lamar',
+    genre: 'Rap',
+  ),
+  const Track(
+    id: '3',
+    title: 'Starboy',
+    artist: 'The Weeknd',
+    genre: 'Pop',
+  ),
+  const Track(
+    id: '4',
+    title: 'Titanium',
+    artist: 'David Guetta',
+    genre: 'Electro',
+  ),
+  const Track(
+    id: '5',
+    title: 'Believer',
+    artist: 'Imagine Dragons',
+    genre: 'Rock',
+  ),
+  const Track(
+    id: '6',
+    title: 'Lose Yourself',
+    artist: 'Eminem',
+    genre: 'Rap',
+  ),
+  const Track(
+    id: '7',
+    title: 'One More Time',
+    artist: 'Daft Punk',
+    genre: 'Electro',
+  ),
+  const Track(
+    id: '8',
+    title: 'Smells Like Teen Spirit',
+    artist: 'Nirvana',
+    genre: 'Rock',
+  ),
+  const Track(
+    id: '9',
+    title: 'Levitating',
+    artist: 'Dua Lipa',
+    genre: 'Pop',
+  ),
+  const Track(
+    id: '10',
+    title: 'Can’t Hold Us',
+    artist: 'Macklemore & Ryan Lewis',
+    genre: 'Rap',
+  ),
+  const Track(
+    id: '11',
+    title: 'Animals',
+    artist: 'Martin Garrix',
+    genre: 'Electro',
+  ),
+  const Track(
+    id: '12',
+    title: 'Numb',
+    artist: 'Linkin Park',
+    genre: 'Rock',
+  ),
+  const Track(
+    id: '13',
+    title: 'Stay',
+    artist: 'The Kid LAROI & Justin Bieber',
+    genre: 'Pop',
+  ),
+  const Track(
+    id: '14',
+    title: 'SICKO MODE',
+    artist: 'Travis Scott',
+    genre: 'Rap',
+  ),
+  const Track(
+    id: '15',
+    title: 'Wake Me Up',
+    artist: 'Avicii',
+    genre: 'Electro',
+  ),
+];
